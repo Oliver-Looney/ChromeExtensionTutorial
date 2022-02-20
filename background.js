bebd5dbd0abcc6bd85ec62ddc64487bc32484df1
Oliver Looney <oliverjlooney@gmail.com>
2022-02-20 16:31:18 +0000

    color change working

--- background.js
+++ background.js
@@ -2,5 +2,5 @@ let color = "#3AA757";
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({color});
-    console.log("Default background color set to ${color}");
+    console.log(`Default background color set to ${color}`);
 });
