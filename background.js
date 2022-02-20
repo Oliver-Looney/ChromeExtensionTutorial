413c4786cd5b1ae382525c6b0930428cf5f381ac
Oliver Looney <oliverjlooney@gmail.com>
2022-02-20 16:10:25 +0000

    added icons

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 let color = "#3AA757";
-console.log(color);
+
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({color});
     console.log("Default background color set to ${color}");
